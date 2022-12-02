710edec6068f91a531201dece050769205c6a988
dibesfer <dibesfer@gmail.com>
2022-12-02 20:45:33 +0100

    Create GameManager.cs

---
+++ GameManager.cs
@@ -0,0 +1,5 @@
+//GameManager is a singleton script for storing 
+// important and persistant player data like 
+// user life, level, character selecter
+// Commonly used functions on every game, in each level/scene.
+// play, plause, quit...
